6dbfa9f5b70743de75d578fe79e7e27e3de21c8f
GADAL PRASAD REDDY <gprasadreddy008@gmail.com>
2025-11-16 08:30:32 +0530

    simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Simple Interest Calculator
+
+echo "Enter Principal Amount:"
+read principal
+echo "Enter Rate of Interest (per annum):"
+read rate
+echo "Enter Time (in years):"
+read time
+
+# Calculate simple interest
+simple_interest=$(echo "scale=2; $principal * $rate * $time / 100" | bc)
+
+echo "Simple Interest = $simple_interest"
